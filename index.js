e2679a9e3c4d573ac046d347707d25fd9bc97393
Ville Paananen <villepaananen@outlook.com>
2020-03-02 20:48:00 +0200

    3.3 done

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 
 let persons = [
-  { name: "Arto Hellas", number: "040-123456" },
-  { name: "Ada Lovelace", number: "49-33-5323523" }
+  { name: "Arto Hellas", number: "040-123456", id: 0 },
+  { name: "Ada Lovelace", number: "49-33-5323523", id: 1 },
+  { name: "Dan Abramov", number: "12-43-234345", id: 2 },
+  { name: "Mary Poppendieck", number: "39-23-6423122", id: 3 }
 ];
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) res.json(person);
+  else res.status(404).send(`No person found with an id of ${id}.`);
 });
 
 app.get("/info", (req, res) => {
