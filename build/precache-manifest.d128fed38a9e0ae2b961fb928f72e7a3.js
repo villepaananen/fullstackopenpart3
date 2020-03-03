0e03d45bf718e05d1c5659c32ec1d074b90c5f65
Ville Paananen <villepaananen@outlook.com>
2020-03-03 20:16:09 +0200

    added build

---
+++ build/precache-manifest.d128fed38a9e0ae2b961fb928f72e7a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc7403fb70ab2d3566700b6fe2202b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8367ce5005431b37c811",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "a4ff7f15b0a9c86bc646",
+    "url": "/static/js/2.b60c7687.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b60c7687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8367ce5005431b37c811",
+    "url": "/static/js/main.674cc5fa.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token("body", function(req, res) {
 
 const app = express();
 app.use(express.json());
+app.use(express.static("build"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
