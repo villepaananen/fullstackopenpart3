9be3cc248ab85456c1fc27d767bcdca0a6ad28ec
Ville Paananen <villepaananen@outlook.com>
2020-03-03 20:04:21 +0200

    3.9 done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 morgan.token("body", function(req, res) {
   return JSON.stringify(req.body);
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+app.use(cors());
 
 let persons = [
   { name: "Arto Hellas", number: "040-123456", id: 0 },
@@ -63,7 +65,7 @@ app.get("/info", (req, res) => {
     .send(`Phonebook has info for ${persons.length} people.\n\n${new Date()}`);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
