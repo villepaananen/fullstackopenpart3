370f4d39ed104dc0de3884342671af5cfc66ce02
Ville Paananen <villepaananen@outlook.com>
2020-03-03 19:45:21 +0200

    3.8 done

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
+
+morgan.token("body", function(req, res) {
+  return JSON.stringify(req.body);
+});
+
 const app = express();
 app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   { name: "Arto Hellas", number: "040-123456", id: 0 },
