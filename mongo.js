316c53ec632576224c4d2cefdd01dd09d956fe6e
Ville Paananen <villepaananen@outlook.com>
2020-03-18 20:11:11 +0200

    3.12 done

--- mongo.js
+++ mongo.js
@@ -6,32 +6,36 @@ if (process.argv.length < 3) {
 }
 
 const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
 
 const url = `mongodb+srv://fullstack:${password}@cluster0-k2c8f.mongodb.net/test?retryWrites=true&w=majority`;
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const personSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean
+  name: String,
+  number: String
 });
 
 const Person = mongoose.model("Person", personSchema);
 
 const person = new Person({
-  name: "HTML is Easy",
-  number: new Date()
+  name: name,
+  number: number
 });
 
 if (process.argv.length > 3) {
   person.save().then(response => {
-    console.log("note saved!");
+    console.log(`added ${person.name} number ${person.number} to phonebook`);
     mongoose.connection.close();
   });
 } else {
-  person.find().then(response => {
-    console.log("note saved!");
+  Person.find({}).then(response => {
+    console.log("Phonebook:");
+    response.forEach(person => {
+      console.log(`${person.name} ${person.number}`);
+    });
     mongoose.connection.close();
   });
 }
